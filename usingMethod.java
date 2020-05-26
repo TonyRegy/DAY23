750b61162381d337ed34d9e0b10e2b59116a2fae
TonyRegy <tonyregy@gmail.com>
2020-05-26 12:15:26 +0530

    using method

---
+++ usingMethod.java
@@ -0,0 +1,41 @@
+ublic class usingMethod{
+	public static final int IS_FULL_TIME=1;
+ 	public static final int IS_PART_TIME=0;
+	public static final int RATE_PER_HOUR=20;
+   public static final int NUMBER_OF_WORKING_DAYS=10;
+	public static final int HOURS_PER_MONTH=20;
+	public static int WORK_HOURS=0;
+	public static int WAGE=0,totalhrs=0,totaldays=0;
+	public static double empCheck=0;
+
+	public static void main(String[] args){
+	emppresentcheck();
+	calculatewage(WORK_HOURS);
+}
+	public static int emppresentcheck(){
+	while(totaldays<=NUMBER_OF_WORKING_DAYS && totalhrs<=HOURS_PER_MONTH){
+	totaldays++;
+	totalhrs++;
+        empCheck=Math.floor(Math.random() * 10) % 3;
+        switch((int)empCheck){
+        case IS_FULL_TIME:
+        WORK_HOURS=8;
+        break;
+        case IS_PART_TIME:
+        WORK_HOURS=4;
+        break;
+        default:
+        WORK_HOURS=0;
+        break;
+ 	}
+	}
+	System.out.println("employee worked hours:"+totalhrs);
+	System.out.println("employee worked days:"+totaldays);
+	return WORK_HOURS;
+	}
+	public static void calculatewage(int WORK_HOURS){
+	WAGE=WORK_HOURS*RATE_PER_HOUR;
+	System.out.println("emply wage is:"+WAGE);
+
+	}
+	}
